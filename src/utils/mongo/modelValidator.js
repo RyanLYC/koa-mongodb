df0c409a35fb081af77c65f10cfde99e6fa111d7
Ryan <656040874@qq.com>
2022-12-01 17:06:06 +0800

    feat: 增加工具类 待验证使用

---
+++ src/utils/mongo/modelValidator.js
@@ -0,0 +1,26 @@
+export const checkEmpty = (model, param) => {
+  model.path(`${param}`).validate((val) => {
+    if (val.length < 0 || val === " ") {
+      return false;
+    } else {
+      return true;
+    }
+  }, `${param}字段不能为空`);
+};
+
+export const formatFilter = (filterField) => {
+  let result = {};
+  if (filterField instanceof Array) {
+    throw `can not recieve a array`;
+  }
+  for (let key in filterField) {
+    if (
+      filterField[key] !== "" &&
+      filterField[key] !== null &&
+      filterField[key] !== undefined
+    ) {
+      result[key] = filterField[key];
+    }
+  }
+  return result;
+};
